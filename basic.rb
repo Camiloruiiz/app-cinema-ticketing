cf63bc3bac3cb1f5f2127ad956e18c1d61e663a6
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-01-28 18:15:22 +0100

    Try with heroku config

--- basic.rb
+++ basic.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'pony'
 
 SITE_TITLE = "Cinelandia"
 SITE_DESCRIPTION = "Las mejores Películas del mundo mundial."
@@ -15,8 +16,7 @@ get'/compraform' do
 end
 
 post '/confirmation' do
-  require 'pony'
-  
+
   name = params[:nombre]
   apellido = params[:apellido]
   correo = params[:correo]
@@ -24,22 +24,24 @@ post '/confirmation' do
   listfilms = params[:listfilms]
   id = 165
   
-  Pony.mail(
-      from: 'noreply@cinelandia.com',
-      to: correo,
-      subject: 'Confirmación compra de Ticket ' + name,
-      body: 'Ingresa al siguiente link: http://127.0.0.1:9393/' + name.gsub(/\s/,'-') + '/' + listfilms.gsub(/\s/,'-') + '/' + id.to_s),
-      port: '587',
-      via: :smtp,
-      via_options: { 
-        :address              => 'smtp.sendgrid.net', 
-        :port                 => '587', 
-        :enable_starttls_auto => true, 
-        :user_name            => ENV['SENDGRID_USERNAME'], 
-        :password             => ENV['SENDGRID_PASSWORD'], 
-        :authentication       => :plain, 
-        :domain               => 'heroku.com'
-      })
+  Pony.options = {
+    
+    :from => 'noreply@esquemacreativo.com',
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :domain => 'heroku.com',
+      :enable_starttls_auto => true,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :enable_starttls_auto => true
+    },
+  }
+  
+  Pony.mail(:subject=> 'Confirmación compra de Ticket ' + name, :to => correo, :body => 'Ingresa al siguiente link: http://127.0.0.1:9393/' + name.gsub(/\s/,'-') + '/' + listfilms.gsub(/\s/,'-') + '/' + id.to_s)
+  
   erb :confirmation , :locals => {'name' => name, 'apellido' => apellido, 'correo' => correo, 'telefono' => telefono, 'film' => listfilms}
 end
 
