6aba5552adbfa7782c84b3cab92aecd11c99258a
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-01-28 22:47:03 +0100

    Config mail

--- basic.rb
+++ basic.rb
@@ -24,22 +24,6 @@ post '/confirmation' do
   listfilms = params[:listfilms]
   id = 165
   
-  Pony.options = {
-    
-    :from => 'noreply@esquemacreativo.com',
-    :via => :smtp,
-    :via_options => {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
-      :domain => 'heroku.com',
-      :enable_starttls_auto => true,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-      :enable_starttls_auto => true
-    },
-  }
-  
   Pony.mail(:subject=> 'Confirmación compra de Ticket ' + name, :to => correo, :body => 'Ingresa al siguiente link: http://127.0.0.1:9393/' + name.gsub(/\s/,'-') + '/' + listfilms.gsub(/\s/,'-') + '/' + id.to_s)
   
   erb :confirmation , :locals => {'name' => name, 'apellido' => apellido, 'correo' => correo, 'telefono' => telefono, 'film' => listfilms}
