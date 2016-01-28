3c98aff24e654ab326ed7fe2e9ae14c92f99cf77
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-01-28 17:35:39 +0100

    Config Pony

--- basic.rb
+++ basic.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'pony'
 
 SITE_TITLE = "Cinelandia"
 SITE_DESCRIPTION = "Las mejores Películas del mundo mundial."
@@ -16,7 +15,8 @@ get'/compraform' do
 end
 
 post '/confirmation' do
-
+  require 'pony'
+  
   name = params[:nombre]
   apellido = params[:apellido]
   correo = params[:correo]
@@ -24,8 +24,22 @@ post '/confirmation' do
   listfilms = params[:listfilms]
   id = 165
   
-  Pony.mail(:to => correo, :from => 'noreply@cinelandia.com', :subject => 'Confirmación compra de Ticket ' + name, :body => 'Ingresa al siguiente link: http://127.0.0.1:9393/' + name.gsub(/\s/,'-') + '/' + listfilms.gsub(/\s/,'-') + '/' + id.to_s)
-  
+  Pony.mail(
+      from: 'noreply@cinelandia.com',
+      to: correo,
+      subject: 'Confirmación compra de Ticket ' + name,
+      body: 'Ingresa al siguiente link: http://127.0.0.1:9393/' + name.gsub(/\s/,'-') + '/' + listfilms.gsub(/\s/,'-') + '/' + id.to_s),
+      port: '587',
+      via: :smtp,
+      via_options: { 
+        :address              => 'smtp.sendgrid.net', 
+        :port                 => '587', 
+        :enable_starttls_auto => true, 
+        :user_name            => ENV['SENDGRID_USERNAME'], 
+        :password             => ENV['SENDGRID_PASSWORD'], 
+        :authentication       => :plain, 
+        :domain               => 'heroku.com'
+      })
   erb :confirmation , :locals => {'name' => name, 'apellido' => apellido, 'correo' => correo, 'telefono' => telefono, 'film' => listfilms}
 end
 
