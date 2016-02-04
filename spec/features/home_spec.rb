31af9f850149317098f9d0527add30a6c3bc655c
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-02-04 17:27:10 +0100

    Config Test with capybara

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,10 +1,8 @@
-
-require_relative '../../app/basic'
 require 'spec_helper.rb'
 
 describe 'Home', :type => :feature do
   it 'responds with successful status' do
     visit '/'
-    page.status_code.should == 200
+    expect(page.status_code).to eq(200)
   end
 end
