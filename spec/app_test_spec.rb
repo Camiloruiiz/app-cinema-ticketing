13d919111150ee82755f31dda973a046c4d4f9bd
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-02-09 18:16:17 +0100

    test: Save records in the hash

--- spec/app_test_spec.rb
+++ spec/app_test_spec.rb
@@ -10,4 +10,21 @@ describe 'App ticketing Cinema' do
       #Assert
       expect(RecordsManagement.db).to eq({0=>"init"})
     end
+
+    it 'Save records in the hash' do
+      #Arrange
+      params = {
+        "name"=>"Name Test",
+        "lastname"=>"LastName Test",
+        "mail"=>"test@test.com",
+        "phone"=>"123456",
+        "list_films"=>"Test."
+      }
+      #Act
+      RecordsManagement.new
+      RecordsManagement.new.save(params)
+      #Assert
+      expect(RecordsManagement.db).to eq({0=>"init", 1=>{"name"=>"Name Test", "lastname"=>"LastName Test", "mail"=>"test@test.com", "phone"=>"123456", "list_films"=>"Test."}})
+    end
+  end
 end
