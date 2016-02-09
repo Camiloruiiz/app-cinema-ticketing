2b535571468b682f2bab4cb5a37f73290e7e99e2
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-02-09 16:29:19 +0100

    Change test's it

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper.rb'
 
-describe 'Home', :type => :feature do
-  it 'responds with successful status' do
-    visit '/'
-    expect(page.status_code).to eq(200)
+describe 'Ticketing', :type => :feature do
+
+  it 'responds with Home page' do
+    visit '/' #Arrange
+    #Act
+    expect(page).to have_title "Cinelandia" #Assert
   end
-  
-  it 'click in the button purchase in /form' do
-  	visit '/'
-  	click_on('buy_tickets')
-  	expect(page.status_code).to eq(200)
+
+  it 'click in the button purchase' do
+  	visit '/' #Arrange
+
+  	click_on('buy_tickets') #Act
+
   	expect(page).to have_content('Book your tickets:')
   end
-  
+
   it 'film form in /form' do
-  	visit '/'
-  	click_on('buy_tickets')
-  	expect(page.status_code).to eq(200)
-  	expect(page).to have_content('Book your tickets:')
+  	#Arrange
+  	Pony.override_options = { :via => :test }
+  	visit '/form'
+
+  	#Act
   	fill_in('name', :with => 'Camilo')
   	fill_in('lastname', :with => 'Ruiz')
   	fill_in('mail', :with => 'camilorojas@esquemacreativo.com')
   	select('Inside Out', :from => 'list_films')
   	click_on('send')
-  	expect(page.status_code).to eq(200)
+
+  	#Assert
+  	expect(page).to have_title "Confirmation"
   	expect(page).to have_content('Hello Camilo Ruiz, your ticket to Inside Out It was successfully generated, you will soon receive an email with the details of your purchase')
   end
 end
