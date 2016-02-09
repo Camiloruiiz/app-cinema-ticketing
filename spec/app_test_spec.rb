8121a4389f97a0ce0eef664abb84839f52fec2fa
CamiloRuiiz <camilorojas@esquemacreativo.com>
2016-02-09 17:56:40 +0100

    Test: The class RecordsManagement is defined

---
+++ spec/app_test_spec.rb
@@ -0,0 +1,8 @@
+describe 'App ticketing Cinema' do
+  context 'Generating ticket' do
+    it 'The class RecordsManagement is defined' do
+      expect(RecordsManagement.new.nil?).to eq(false)
+    end
+    
+  end
+end
