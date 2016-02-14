c60aeed89d0fc054e91cac8f8cdd7d1cac5e1190
Camiloruiiz <camilorojas@esquemacreativo.com>
2016-02-14 13:24:57 +0100

    arrange the test with the new specifications

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -8,20 +8,13 @@ describe 'Front-end ticketing Cinema', :type => :feature do
     expect(page).to have_title "Cinelandia" #Assert
   end
 
-  it 'click in the button purchase' do
+  it 'click in the button purchase and film form' do
   	visit '/' #Arrange
+    Pony.override_options = { :via => :test }
 
   	click_on('buy_tickets') #Act
 
-  	expect(page).to have_content('Book your tickets:')
-  end
-
-  it 'film form in /form' do
-  	#Arrange
-  	Pony.override_options = { :via => :test }
-  	visit '/form'
-
-  	#Act
+    #Act
   	fill_in('name', :with => 'Camilo')
   	fill_in('lastname', :with => 'Ruiz')
   	fill_in('mail', :with => 'camilorojas@esquemacreativo.com')
@@ -30,6 +23,6 @@ describe 'Front-end ticketing Cinema', :type => :feature do
 
   	#Assert
   	expect(page).to have_title "Confirmation"
-  	expect(page).to have_content('Hello Camilo Ruiz, your ticket to Inside Out It was successfully generated, you will soon receive an email with the details of your purchase')
+  	expect(page).to have_content('Hello Camilo Ruiz, your ticket to Inside Out It was successfully generated, you will soon receive an email with the details of your purchase.')
   end
 end
